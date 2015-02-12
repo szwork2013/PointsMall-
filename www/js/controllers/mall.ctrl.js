9781802fd1261caa21f43b3060d8fd951b93cd08
abjia <daohao1914>
2015-02-12 14:45:34 +0800

    测试

--- www/js/controllers/mall.ctrl.js
+++ www/js/controllers/mall.ctrl.js
@@ -3,4 +3,14 @@ PointMall.controller("MallCtrl",function($state,$rootScope){
         $rootScope.go = function(stateName){
             $state.go(stateName);
         }
+
+        $rootScope.goCicadaVal = function(){
+            console.log(window.cicadaStore);
+            window.cicadaStore.gotoActiveValue();
+
+        }
+
+    $rootScope.goCicadaBack = function(){
+        window.cicadaStore.back()
+    }
 });
